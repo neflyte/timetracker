fe7404e83c62db78c5b97571cd99489fa0af85f3
alan lew <alan@ethereal.cc>
2021-05-23 15:38:04 -0400

    Move Task Editor into its own first-class widget; Start wiring up events in the task editor widget

--- internal/ui/gui/manage_window.go
+++ internal/ui/gui/manage_window.go
@@ -6,10 +6,10 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/neflyte/timetracker/internal/logger"
 	"github.com/neflyte/timetracker/internal/models"
+	"github.com/neflyte/timetracker/internal/ui/gui/widgets"
 	"github.com/rs/zerolog"
 )
 
@@ -27,30 +27,19 @@ type ManageWindow interface {
 type manageWindow struct {
 	Log zerolog.Logger
 
-	App              *fyne.App
-	Window           fyne.Window
-	Container        *fyne.Container
-	ListTasks        *widget.List
-	BtnEditSave      *widget.Button
-	BtnEditCancel    *widget.Button
-	LabelSynopsis    *widget.Label
-	LabelDescription *widget.Label
-	EntrySynopsis    *widget.Entry
-	EntryDescription *widget.Entry
+	App        *fyne.App
+	Window     fyne.Window
+	Container  *fyne.Container
+	ListTasks  *widget.List
+	TaskEditor *widgets.TaskEditor
 
-	BindTaskList            binding.ExternalStringList
-	BindTaskEditSynopsis    binding.ExternalString
-	BindTaskEditDescription binding.ExternalString
+	BindTaskList binding.ExternalStringList
 
-	isEditing           bool
-	isDirty             bool
-	selectedTaskID      widget.ListItemID
-	taskList            []string
-	taskEditSynopsis    string
-	taskEditDescription string
+	isEditing      bool
+	isDirty        bool
+	selectedTaskID widget.ListItemID
+	taskList       []string
 }
-
-// TODO: Make the task editor a first-class widget
 
 func NewManageWindow(app fyne.App) ManageWindow {
 	mw := &manageWindow{
@@ -67,59 +56,19 @@ func NewManageWindow(app fyne.App) ManageWindow {
 func (m *manageWindow) Init() {
 	// setup bindings
 	m.BindTaskList = binding.BindStringList(&m.taskList)
-	m.BindTaskEditSynopsis = binding.BindString(&m.taskEditSynopsis)
-	m.BindTaskEditDescription = binding.BindString(&m.taskEditDescription)
 	// setup widgets
+	m.TaskEditor = widgets.NewTaskEditor(m.doEditSave, m.doEditCancel)
 	m.ListTasks = widget.NewListWithData(m.BindTaskList, m.listTasksCreateItem, m.listTasksUpdateItem)
 	m.ListTasks.OnSelected = m.taskWasSelected
-	m.LabelSynopsis = widget.NewLabel("Synopsis:")
-	m.EntrySynopsis = widget.NewEntryWithData(m.BindTaskEditSynopsis)
-	m.EntrySynopsis.SetPlaceHolder("enter the task synopsis here")
-	m.LabelDescription = widget.NewLabel("Description:")
-	m.EntryDescription = widget.NewEntryWithData(m.BindTaskEditDescription)
-	m.EntryDescription.MultiLine = true
-	m.EntryDescription.SetPlaceHolder("enter the task description here")
-	m.BtnEditSave = widget.NewButtonWithIcon("Save", theme.ConfirmIcon(), m.doEditSave)
-	m.BtnEditCancel = widget.NewButtonWithIcon("Cancel", theme.CancelIcon(), m.doEditCancel)
 	// setup layout
-	m.Container = container.NewPadded(container.NewHSplit(
-		container.NewPadded(container.NewMax(m.ListTasks)),
-		container.NewPadded(container.NewBorder(
-			nil,
-			container.NewHBox(m.BtnEditCancel, m.BtnEditSave),
-			nil,
-			nil,
-			container.NewVBox(
-				m.LabelSynopsis, m.EntrySynopsis,
-				m.LabelDescription, m.EntryDescription,
-			),
-		)),
+	m.Container = container.NewMax(container.NewHSplit(
+		container.NewPadded(m.ListTasks),
+		container.NewPadded(m.TaskEditor),
 	))
 	m.Window.SetCloseIntercept(m.Hide)
 	m.Window.SetContent(m.Container)
 	m.Window.SetFixedSize(true)
 	m.Window.Resize(MinimumWindowSize)
-	// Load window data in a goroutine
-	// go m.InitWindowData()
-}
-
-func (m *manageWindow) InitWindowData() {
-	log := m.Log.With().Str("func", "InitWindowData").Logger()
-	log.Trace().Msg("started")
-	tasks, err := models.Task(new(models.TaskData)).LoadAll(false)
-	if err != nil {
-		log.Err(err).Msg("error reading all tasks")
-		return
-	}
-	log.Debug().Msgf("read %d tasks", len(tasks))
-	for _, task := range tasks {
-		log.Debug().Msgf("task=%s", task.String())
-		err = m.BindTaskList.Append(task.Synopsis)
-		if err != nil {
-			log.Err(err).Msgf("error appending task %s", task.String())
-		}
-	}
-	log.Trace().Msg("finished")
 }
 
 func (m *manageWindow) Get() manageWindow {
@@ -160,14 +109,21 @@ func (m *manageWindow) refreshTasks() {
 		if err != nil {
 			log.Err(err).Msgf("error appending task %s", task.String())
 		}
-		m.ListTasks.Refresh()
 	}
 }
 
 func (m *manageWindow) doEditSave() {
-	if !m.isDirty {
+	log := m.Log.With().Str("function", "doEditSave").Logger()
+	if !m.TaskEditor.IsDirty() {
 		return
 	}
+	err := m.saveChanges()
+	if err != nil {
+		// TODO: show error dialog
+		log.Err(err).Msg("error saving changes to task")
+		return
+	}
+	m.doneEditing()
 }
 
 func (m *manageWindow) doEditCancel() {
@@ -180,7 +136,7 @@ func (m *manageWindow) doEditCancel() {
 		return
 	}
 	// check if we are dirty and prompt to save if we are
-	if m.isDirty {
+	if m.TaskEditor.IsDirty() {
 		dialog.NewConfirm(
 			"Save changes?",
 			"You have unsaved changes. Would you like to save them?",
@@ -204,42 +160,30 @@ func (m *manageWindow) doEditCancel() {
 
 func (m *manageWindow) saveChanges() error {
 	log := m.Log.With().Str("function", "saveChanges").Logger()
-	if m.isEditing && m.isDirty {
-		newTaskSynopsis, err := m.BindTaskEditSynopsis.Get()
-		if err != nil {
-			// TODO: show error dialog
-			log.Err(err).Msg("error getting synopsis")
-			return err
-		}
-		newTaskDescription, err := m.BindTaskEditDescription.Get()
-		if err != nil {
-			// TODO: show error dialog
-			log.Err(err).Msg("error getting description")
-			return err
+	if m.isEditing && m.TaskEditor.IsDirty() {
+		dirtyTask := m.TaskEditor.GetDirtyTask()
+		if dirtyTask == nil {
+			log.Error().Msg("dirty task is nil; this is unexpected")
+			return errors.New("dirty task is nil")
 		}
 		taskSyn := m.taskList[m.selectedTaskID]
 		td := new(models.TaskData)
 		td.Synopsis = taskSyn
-		err = td.Load(false)
+		err := td.Load(false)
 		if err != nil {
 			// TODO: show error dialog
 			log.Err(err).Msgf("error loading task with synopsis %s", taskSyn)
 			return err
 		}
-		td.Synopsis = newTaskSynopsis
-		td.Description = newTaskDescription
+		td.Synopsis = dirtyTask.Synopsis
+		td.Description = dirtyTask.Description
 		err = td.Update(false)
 		if err != nil {
 			// TODO: show error dialog
 			log.Err(err).Msgf("error updating task %s", td.String())
 			return err
 		}
-		err = m.BindTaskList.SetValue(m.selectedTaskID, td.Synopsis)
-		if err != nil {
-			// TODO: show error dialog
-			log.Err(err).Msgf("error updating task list")
-			return err
-		}
+		m.refreshTasks()
 		return nil
 	}
 	return errors.New("task is not being edited or task is not dirty")
@@ -251,19 +195,12 @@ func (m *manageWindow) doneEditing() {
 		return
 	}
 	m.isEditing = false
-	m.isDirty = false
 	m.ListTasks.Unselect(m.selectedTaskID)
 	m.selectedTaskID = noSelectionIndex
-	err := m.BindTaskEditSynopsis.Set("")
+	err := m.TaskEditor.SetTask(new(models.TaskData))
 	if err != nil {
-		log.Err(err).Msg("error clearing synopsis")
+		log.Err(err).Msg("error setting task to empty task")
 	}
-	err = m.BindTaskEditDescription.Set("")
-	if err != nil {
-		log.Err(err).Msg("error clearing description")
-	}
-	m.BtnEditSave.Disable()
-	m.BtnEditCancel.Disable()
 	m.toggleEditWidgets(false)
 }
 
@@ -294,22 +231,16 @@ func (m *manageWindow) taskWasSelected(id widget.ListItemID) {
 		log.Err(err).Msgf("error loading task with synopsis %s", taskSyn)
 		return
 	}
-	err = m.BindTaskEditSynopsis.Set(td.Synopsis)
+	err = m.TaskEditor.SetTask(td)
 	if err != nil {
-		log.Err(err).Msg("error binding synopsis")
+		log.Err(err).Msg("error setting task")
 	}
-	err = m.BindTaskEditDescription.Set(td.Description)
-	if err != nil {
-		log.Err(err).Msg("error binding description")
-	}
-	m.BtnEditCancel.Enable()
-	m.BtnEditSave.Disable()
 }
 
 // TODO: Make a first-class widget for the task list item
 
 func (m *manageWindow) listTasksCreateItem() fyne.CanvasObject {
-	return widget.NewCard("", "", widget.NewLabel(""))
+	return widget.NewCard("", "", container.NewPadded(widget.NewLabel("")))
 }
 
 func (m *manageWindow) listTasksUpdateItem(item binding.DataItem, canvasObject fyne.CanvasObject) {
@@ -336,23 +267,14 @@ func (m *manageWindow) listTasksUpdateItem(item binding.DataItem, canvasObject f
 	log.Trace().Msgf("setting title=%s, subtitle=%s", td.Synopsis, td.Description)
 	taskCard.SetTitle(td.Synopsis)
 	// TODO: trim subtitle to 64 chars; use ellipsis if >64 chars
-	taskCard.Content.(*widget.Label).SetText(td.Description)
+	taskCard.SetSubTitle(td.Description)
+	//taskCard.Content.(*fyne.Container).Objects[0].(*widget.Label).SetText(td.Description)
 }
 
 func (m *manageWindow) toggleEditWidgets(show bool) {
 	if show {
-		m.LabelSynopsis.Show()
-		m.LabelDescription.Show()
-		m.EntrySynopsis.Show()
-		m.EntryDescription.Show()
-		m.BtnEditSave.Show()
-		m.BtnEditCancel.Show()
+		m.TaskEditor.Show()
 		return
 	}
-	m.LabelSynopsis.Hide()
-	m.LabelDescription.Hide()
-	m.EntrySynopsis.Hide()
-	m.EntryDescription.Hide()
-	m.BtnEditSave.Hide()
-	m.BtnEditCancel.Hide()
+	m.TaskEditor.Hide()
 }
--- internal/ui/gui/widgets/task_editor.go
+++ internal/ui/gui/widgets/task_editor.go
@@ -2,32 +2,165 @@ package widgets
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/neflyte/timetracker/internal/models"
 )
 
 type TaskEditor struct {
 	widget.BaseWidget
+	taskSynopsis           string
+	taskSynopsisBinding    binding.ExternalString
+	taskDescription        string
+	taskDescriptionBinding binding.ExternalString
+	taskData               *models.TaskData
+	isDirtyBinding         binding.ExternalBool
+	isDirty                bool
+	saveCB                 func()
+	closeCB                func()
+}
+
+func NewTaskEditor(saveCallback func(), closeCallback func()) *TaskEditor {
+	te := new(TaskEditor)
+	te.ExtendBaseWidget(te)
+	te.saveCB = saveCallback
+	te.closeCB = closeCallback
+	te.isDirtyBinding = binding.BindBool(&te.isDirty)
+	te.taskSynopsisBinding = binding.BindString(&te.taskSynopsis)
+	te.taskSynopsisBinding.AddListener(binding.NewDataListener(func() {
+		newSyn, err := te.taskSynopsisBinding.Get()
+		if err == nil {
+			if te.taskData != nil {
+				te.isDirty = te.taskData.Synopsis != newSyn
+			}
+		}
+	}))
+	te.taskDescriptionBinding = binding.BindString(&te.taskDescription)
+	te.taskDescriptionBinding.AddListener(binding.NewDataListener(func() {
+		newDesc, err := te.taskDescriptionBinding.Get()
+		if err == nil {
+			if te.taskData != nil {
+				te.isDirty = te.taskData.Description != newDesc
+			}
+		}
+	}))
+	te.taskData = new(models.TaskData)
+	return te
+}
+
+func (te *TaskEditor) GetTask() *models.TaskData {
+	return te.taskData
+}
+
+func (te *TaskEditor) SetTask(task *models.TaskData) error {
+	te.taskData = task
+	if te.taskData != nil {
+		err := te.taskSynopsisBinding.Set(te.taskData.Synopsis)
+		if err != nil {
+			return err
+		}
+		err = te.taskDescriptionBinding.Set(te.taskData.Description)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (te *TaskEditor) GetDirtyTask() *models.TaskData {
+	if !te.IsDirty() {
+		return te.GetTask()
+	}
+	dirty := new(models.TaskData)
+	dirty.ID = te.GetTask().ID
+	dirty.Synopsis = te.taskSynopsis
+	dirty.Description = te.taskDescription
+	dirty.CreatedAt = te.GetTask().CreatedAt
+	dirty.UpdatedAt = te.GetTask().UpdatedAt
+	return dirty
+}
+
+func (te *TaskEditor) IsDirty() bool {
+	return te.isDirty
 }
 
 func (te *TaskEditor) CreateRenderer() fyne.WidgetRenderer {
 	te.ExtendBaseWidget(te)
-	return &taskEditorRenderer{}
+	r := &taskEditorRenderer{
+		taskData:         te.taskData,
+		synopsisLabel:    widget.NewLabel("Synopsis:"),
+		synopsisEntry:    widget.NewEntryWithData(te.taskSynopsisBinding),
+		descriptionLabel: widget.NewLabel("Description:"),
+		descriptionEntry: widget.NewEntryWithData(te.taskDescriptionBinding),
+		saveCallback:     te.saveCB,
+		closeCallback:    te.closeCB,
+	}
+	r.synopsisEntry.SetPlaceHolder("enter the task synopsis here")
+	// TODO: write a function that updates the enabled status of buttons when an entry changes
+	r.synopsisEntry.OnChanged = func(changed string) {
+		if r.taskData != nil {
+			isDirty := changed != r.taskData.Synopsis
+			if isDirty {
+				r.saveButton.Enable()
+			} else {
+				r.saveButton.Disable()
+			}
+		}
+	}
+	r.descriptionEntry.SetPlaceHolder("enter the task description here")
+	r.descriptionEntry.MultiLine = true
+	r.fieldContainer = container.NewVBox(
+		r.synopsisLabel, r.synopsisEntry,
+		r.descriptionLabel, r.descriptionEntry,
+	)
+	r.saveButton = widget.NewButtonWithIcon("SAVE", theme.ConfirmIcon(), r.saveCallback)
+	r.closeButton = widget.NewButtonWithIcon("CLOSE", theme.CancelIcon(), r.closeCallback)
+	r.buttonContainer = container.NewCenter(container.NewHBox(r.closeButton, r.saveButton))
+	r.canvasObjects = []fyne.CanvasObject{
+		r.buttonContainer, r.fieldContainer,
+	}
+	r.layout = layout.NewBorderLayout(
+		nil,
+		r.buttonContainer,
+		nil,
+		nil,
+	)
+	return r
 }
 
 type taskEditorRenderer struct {
-	canvasObjects []fyne.CanvasObject
+	canvasObjects    []fyne.CanvasObject
+	layout           fyne.Layout
+	synopsisLabel    *widget.Label
+	synopsisEntry    *widget.Entry
+	descriptionLabel *widget.Label
+	descriptionEntry *widget.Entry
+	saveButton       *widget.Button
+	closeButton      *widget.Button
+	fieldContainer   *fyne.Container
+	buttonContainer  *fyne.Container
+	saveCallback     func()
+	closeCallback    func()
+	taskData         *models.TaskData
 }
 
 func (r *taskEditorRenderer) Destroy() {}
 
-func (r *taskEditorRenderer) Layout(size fyne.Size) {}
+func (r *taskEditorRenderer) Layout(size fyne.Size) {
+	r.layout.Layout(r.Objects(), size)
+}
 
 func (r *taskEditorRenderer) MinSize() fyne.Size {
-	return fyne.NewSize(0, 0)
+	return r.layout.MinSize(r.Objects())
 }
 
 func (r *taskEditorRenderer) Objects() []fyne.CanvasObject {
 	return r.canvasObjects
 }
 
-func (r *taskEditorRenderer) Refresh() {}
+func (r *taskEditorRenderer) Refresh() {
+	r.Layout(r.MinSize())
+}
