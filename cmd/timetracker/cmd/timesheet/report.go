878ae3789f1d93a0dcc902b98f85ce9d79aab394
alan lew <alan@ethereal.cc>
2021-08-15 15:14:09 -0400

    Add CLI functionality to export task report to CSV file; Add editorconfig file with golang defaults

--- cmd/timetracker/cmd/timesheet/report.go
+++ cmd/timetracker/cmd/timesheet/report.go
@@ -1,6 +1,7 @@
 package timesheet
 
 import (
+	"encoding/csv"
 	"errors"
 	"fmt"
 	"github.com/alexeyco/simpletable"
@@ -9,8 +10,13 @@ import (
 	"github.com/neflyte/timetracker/internal/models"
 	"github.com/neflyte/timetracker/internal/ui/cli"
 	"github.com/spf13/cobra"
+	"os"
 	"strconv"
 	"time"
+)
+
+const (
+	exportFileMode = 0644
 )
 
 var (
@@ -21,14 +27,17 @@ var (
 		Short:   "Report on tasks completed in the specified time period",
 		RunE:    reportTimesheets,
 	}
+	csvTableHeader  = []string{"task_id", "synopsis", "started_on", "duration"}
 	reportStartDate string
 	reportEndDate   string
+	exportCSVFile   string
 )
 
 func init() {
 	ReportCmd.Flags().StringVar(&reportStartDate, "startDate", "", "start date (YYYY-MM-DD)")
 	ReportCmd.Flags().StringVar(&reportEndDate, "endDate", "", "end date (YYYY-MM-DD)")
 	ReportCmd.Flags().BoolVar(&withDeleted, "deleted", false, "include deleted timesheets")
+	ReportCmd.Flags().StringVar(&exportCSVFile, "exportCSV", "", "file to export report in CSV format")
 }
 
 func reportTimesheets(_ *cobra.Command, _ []string) (err error) {
@@ -52,6 +61,38 @@ func reportTimesheets(_ *cobra.Command, _ []string) (err error) {
 	if reportErr != nil {
 		cli.PrintAndLogError(log, reportErr, "error running task report between %s and %s", reportStartDate, reportEndDate)
 		return reportErr
+	}
+	if exportCSVFile != "" {
+		outFile, fileErr := os.OpenFile(exportCSVFile, os.O_CREATE|os.O_RDWR|os.O_TRUNC, exportFileMode)
+		if fileErr != nil {
+			cli.PrintAndLogError(log, fileErr, "error opening output file %s", exportCSVFile)
+			return fileErr
+		}
+		defer func() {
+			closeErr := outFile.Close()
+			if closeErr != nil {
+				cli.PrintAndLogError(log, closeErr, "error closing output file %s", exportCSVFile)
+			}
+		}()
+		csvOut := csv.NewWriter(outFile)
+		defer csvOut.Flush()
+		csvData := make([][]string, 0)
+		csvData = append(csvData, csvTableHeader)
+		for _, taskReportData := range reportData {
+			csvData = append(csvData, []string{
+				fmt.Sprintf("%d", taskReportData.TaskID),
+				taskReportData.TaskSynopsis,
+				taskReportData.StartDate.Time.Format(constants.TimestampDateLayout),
+				taskReportData.Duration().String(),
+			})
+		}
+		writeErr := csvOut.WriteAll(csvData)
+		if writeErr != nil {
+			cli.PrintAndLogError(log, writeErr, "error exporting data to CSV file %s", exportCSVFile)
+			return writeErr
+		}
+		fmt.Printf("Exported %d records to file %s\n", len(reportData), exportCSVFile)
+		return nil
 	}
 	table := simpletable.New()
 	table.Header = &simpletable.Header{
