486f0190a566967f58a6793a0ff726cf737ce4a4
alan lew <alan@ethereal.cc>
2021-08-15 10:19:23 -0400

    Remove separator from report window since it causes widget height to be miscalculated; Fix report date entry validation

--- internal/ui/gui/widgets/date_entry.go
+++ internal/ui/gui/widgets/date_entry.go
@@ -1,37 +1,60 @@
 package widgets
 
 import (
+	"errors"
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
-	"github.com/neflyte/timetracker/internal/logger"
-	"github.com/rs/zerolog"
-)
-
-const (
-	dateEntryMinWidth = 150.0
+	"time"
 )
 
 // DateEntry is an entry widget which accepts a date
 type DateEntry struct {
 	widget.Entry
-	log zerolog.Logger
+
+	minWidth      float32
+	dateFormat    string
+	stringBinding binding.String
 }
 
 // NewDateEntry returns a new DateEntry widget
-func NewDateEntry() *DateEntry {
+func NewDateEntry(minWidth float32, placeholder string, dateFormat string, stringBinding binding.String) *DateEntry {
 	newEntry := &DateEntry{
-		log: logger.GetStructLogger("DateEntry"),
+		dateFormat:    dateFormat,
+		minWidth:      minWidth,
+		stringBinding: stringBinding,
 	}
 	newEntry.ExtendBaseWidget(newEntry)
+	newEntry.PlaceHolder = placeholder
+	if newEntry.stringBinding != nil {
+		newEntry.stringBinding.AddListener(binding.NewDataListener(newEntry.ValidateDate))
+	}
 	return newEntry
 }
 
 // MinSize is the minimum size of the widget
 func (d *DateEntry) MinSize() fyne.Size {
+	d.ExtendBaseWidget(d)
 	entrySize := d.Entry.MinSize()
 	minWidth := entrySize.Width
-	if minWidth < dateEntryMinWidth {
-		minWidth = dateEntryMinWidth
+	if minWidth < d.minWidth {
+		minWidth = d.minWidth
 	}
 	return fyne.NewSize(minWidth, entrySize.Height)
+}
+
+func (d *DateEntry) ValidateDate() {
+	if d.stringBinding != nil {
+		stringValue, err := d.stringBinding.Get()
+		if err != nil {
+			d.SetValidationError(err)
+			return
+		}
+		if stringValue == "" {
+			d.SetValidationError(errors.New("date cannot be empty"))
+			return
+		}
+		_, err = time.Parse(d.dateFormat, stringValue)
+		d.SetValidationError(err)
+	}
 }
