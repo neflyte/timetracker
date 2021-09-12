5264d1ca57e0b132ec6177e70611e5d7b7691f4c
alan lew <alan@ethereal.cc>
2021-09-12 16:58:32 -0400

    Added channels and validators

--- internal/ui/gui/widgets/date_picker.go
+++ internal/ui/gui/widgets/date_picker.go
@@ -16,6 +16,9 @@ import (
 )
 
 const (
+	DatePickerSubmitEventKey = "submit"
+	DatePickerCancelEventKey = "cancel"
+
 	dateStringFormat  = "2006-01-02"
 	minYear           = 0
 	maxYear           = 9999
@@ -23,54 +26,86 @@ const (
 	maxMonth          = 12
 	minDay            = 1
 	maxDay            = 31
-	channelBufferSize = 1
+	channelBufferSize = 2
 )
 
 type DatePicker struct {
-	widget.DisableableWidget
+	widget.BaseWidget
 	fmt.Stringer
 
-	log          zerolog.Logger
-	yearValue    string
-	monthValue   string
-	dayValue     string
-	yearBinding  binding.String
-	monthBinding binding.String
-	dayBinding   binding.String
-	yearChan     chan rxgo.Item
-	monthChan    chan rxgo.Item
-	dayChan      chan rxgo.Item
+	log            zerolog.Logger
+	yearBinding    binding.String
+	monthBinding   binding.String
+	dayBinding     binding.String
+	submitChannel  chan rxgo.Item
+	cancelChannel  chan rxgo.Item
+	observablesMap map[string]rxgo.Observable
 }
 
 func NewDatePicker() *DatePicker {
 	datePicker := &DatePicker{
-		log:          logger.GetStructLogger("DatePicker"),
-		yearBinding:  binding.NewString(),
-		monthBinding: binding.NewString(),
-		dayBinding:   binding.NewString(),
-		yearChan:     make(chan rxgo.Item, channelBufferSize),
-		monthChan:    make(chan rxgo.Item, channelBufferSize),
-		dayChan:      make(chan rxgo.Item, channelBufferSize),
+		log:           logger.GetStructLogger("DatePicker"),
+		yearBinding:   binding.NewString(),
+		monthBinding:  binding.NewString(),
+		dayBinding:    binding.NewString(),
+		submitChannel: make(chan rxgo.Item, channelBufferSize),
+		cancelChannel: make(chan rxgo.Item, channelBufferSize),
 	}
 	datePicker.ExtendBaseWidget(datePicker)
+	datePicker.observablesMap = map[string]rxgo.Observable{
+		DatePickerSubmitEventKey: rxgo.FromEventSource(datePicker.submitChannel),
+		DatePickerCancelEventKey: rxgo.FromEventSource(datePicker.cancelChannel),
+	}
 	timeNow := time.Now()
-	_ = datePicker.yearBinding.Set(strconv.Itoa(timeNow.Year()))
-	_ = datePicker.monthBinding.Set(strconv.Itoa(int(timeNow.Month())))
-	_ = datePicker.dayBinding.Set(strconv.Itoa(timeNow.Day()))
+	yearString := strconv.Itoa(timeNow.Year())
+	err := datePicker.yearBinding.Set(yearString)
+	if err != nil {
+		datePicker.log.Err(err).Msgf("error setting yearBinding to %s", yearString)
+	}
+	monthString := strconv.Itoa(int(timeNow.Month()))
+	err = datePicker.monthBinding.Set(monthString)
+	if err != nil {
+		datePicker.log.Err(err).Msgf("error setting monthBinding to %s", monthString)
+	}
+	dayString := strconv.Itoa(timeNow.Day())
+	err = datePicker.dayBinding.Set(dayString)
+	if err != nil {
+		datePicker.log.Err(err).Msgf("error setting dayBinding to %s", dayString)
+	}
 	return datePicker
 }
 
 func (dp *DatePicker) String() string {
-	return fmt.Sprintf("%s-%s-%s", dp.yearValue, dp.monthValue, dp.dayValue)
+	yearValue, err := dp.yearBinding.Get()
+	if err != nil {
+		dp.log.Err(err).Msg("error getting value from yearBinding")
+		yearValue = "????"
+	}
+	monthValue, err := dp.monthBinding.Get()
+	if err != nil {
+		dp.log.Err(err).Msg("error getting value from monthBinding")
+		monthValue = "??"
+	}
+	dayValue, err := dp.dayBinding.Get()
+	if err != nil {
+		dp.log.Err(err).Msg("error getting value from dayBinding")
+		dayValue = "??"
+	}
+	return fmt.Sprintf("%s-%s-%s", yearValue, monthValue, dayValue)
 }
 
 func (dp *DatePicker) Time() (*time.Time, error) {
-	dateStringValue := fmt.Sprintf("%s-%s-%s", dp.yearValue, dp.monthValue, dp.dayValue)
+	dateStringValue := dp.String()
 	timeValue, err := time.Parse(dateStringFormat, dateStringValue)
 	if err != nil {
 		return nil, err
 	}
 	return &timeValue, nil
+}
+
+// Observables returns a map of Observable objects used by this widget
+func (dp *DatePicker) Observables() map[string]rxgo.Observable {
+	return dp.observablesMap
 }
 
 func (dp *DatePicker) CreateRenderer() fyne.WidgetRenderer {
@@ -81,22 +116,67 @@ func (dp *DatePicker) CreateRenderer() fyne.WidgetRenderer {
 		yearEntry:     widget.NewEntryWithData(dp.yearBinding),
 		monthEntry:    widget.NewEntryWithData(dp.monthBinding),
 		dayEntry:      widget.NewEntryWithData(dp.dayBinding),
+		submitChannel: dp.submitChannel,
+		cancelChannel: dp.cancelChannel,
 		layout:        layout.NewHBoxLayout(),
+	}
+	r.yearEntry.Validator = func(yearValue string) error {
+		yearIntValue, err := strconv.Atoi(yearValue)
+		if err != nil {
+			return err
+		}
+		if yearIntValue < minYear {
+			return fmt.Errorf("year cannot be earlier than %d", minYear)
+		}
+		if yearIntValue > maxYear {
+			return fmt.Errorf("year cannot be later than %d", maxYear)
+		}
+		return nil
 	}
 	r.yearAddButton = widget.NewButtonWithIcon("", theme.MenuDropUpIcon(), r.doAddYear)
 	r.yearSubButton = widget.NewButtonWithIcon("", theme.MenuDropDownIcon(), r.doSubYear)
 	r.yearVBox = container.NewVBox(r.yearAddButton, r.yearSubButton)
+	r.monthEntry.Validator = func(monthValue string) error {
+		monthIntValue, err := strconv.Atoi(monthValue)
+		if err != nil {
+			return err
+		}
+		if monthIntValue < minMonth {
+			return fmt.Errorf("month cannot be earlier than %d", minMonth)
+		}
+		if monthIntValue > maxMonth {
+			return fmt.Errorf("month cannot be later than %d", maxMonth)
+		}
+		return nil
+	}
 	r.monthAddButton = widget.NewButtonWithIcon("", theme.MenuDropUpIcon(), r.doAddMonth)
 	r.monthSubButton = widget.NewButtonWithIcon("", theme.MenuDropDownIcon(), r.doSubMonth)
 	r.monthVBox = container.NewVBox(r.monthAddButton, r.monthSubButton)
+	r.dayEntry.Validator = func(dayValue string) error {
+		dayIntValue, err := strconv.Atoi(dayValue)
+		if err != nil {
+			return err
+		}
+		if dayIntValue < minDay {
+			return fmt.Errorf("day cannot be earlier than %d", minDay)
+		}
+		if dayIntValue > maxDay {
+			return fmt.Errorf("day cannot be later than %d", maxDay)
+		}
+		return nil
+	}
 	r.dayAddButton = widget.NewButtonWithIcon("", theme.MenuDropUpIcon(), r.doAddDay)
 	r.daySubButton = widget.NewButtonWithIcon("", theme.MenuDropDownIcon(), r.doSubDay)
 	r.dayVBox = container.NewVBox(r.dayAddButton, r.daySubButton)
+	r.okButton = widget.NewButtonWithIcon("", theme.ConfirmIcon(), r.doSubmit)
+	r.cancelButton = widget.NewButtonWithIcon("", theme.CancelIcon(), r.doCancel)
 	r.canvasObjects = append(
 		r.canvasObjects,
 		r.yearEntry, r.yearVBox,
 		r.monthEntry, r.monthVBox,
 		r.dayEntry, r.dayVBox,
+		r.cancelButton,
+		r.okButton,
 	)
 	return r
 }
@@ -118,6 +198,10 @@ type datePickerRenderer struct {
 	dayAddButton   *widget.Button
 	daySubButton   *widget.Button
 	dayVBox        *fyne.Container
+	okButton       *widget.Button
+	cancelButton   *widget.Button
+	submitChannel  chan rxgo.Item
+	cancelChannel  chan rxgo.Item
 }
 
 func (d datePickerRenderer) Destroy() {
@@ -216,4 +300,39 @@ func (d *datePickerRenderer) doSubDay() {
 	}
 	dayValue--
 	d.dayEntry.SetText(strconv.Itoa(dayValue))
+}
+
+func (d *datePickerRenderer) validateEntries() error {
+	err := d.yearEntry.Validate()
+	if err != nil {
+		return err
+	}
+	err = d.monthEntry.Validate()
+	if err != nil {
+		return err
+	}
+	err = d.dayEntry.Validate()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *datePickerRenderer) doSubmit() {
+	err := d.validateEntries()
+	if err != nil {
+		d.log.Err(err).Msg("error validating entry fields; cannot submit")
+		return
+	}
+	dateString := fmt.Sprintf(
+		"%s-%s-%s",
+		d.yearEntry.Text,
+		d.monthEntry.Text,
+		d.dayEntry.Text,
+	)
+	d.submitChannel <- rxgo.Of(dateString)
+}
+
+func (d *datePickerRenderer) doCancel() {
+	d.submitChannel <- rxgo.Of(true)
 }
