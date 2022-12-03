003b7eca9d11f1c2f1868e26e04d983c5e12b43e
alan lew <alan@ethereal.cc>
2022-12-03 17:12:46 -0500

    Bump version to v0.3.3; Start creating new task widgets

---
+++ internal/ui/gui/widgets/task.go
@@ -0,0 +1,80 @@
+package widgets
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"github.com/neflyte/timetracker/internal/models"
+)
+
+const (
+	taskDefaultSynopsis = "(?)"
+	taskDefaultDescription
+)
+
+// TaskIntf is the interface to the Task widget
+type TaskIntf interface {
+	Task() models.Task
+	SetTask(newTask models.Task)
+}
+
+// taskImpl is the struct implementation of the TaskIntf interface
+type taskImpl struct {
+	widget.BaseWidget
+	task        models.Task
+	container   *fyne.Container
+	synopsis    *widget.Label
+	description *widget.Label
+}
+
+func NewTask() TaskIntf {
+	return NewTaskWithData(nil)
+}
+
+func NewTaskWithData(taskData models.Task) TaskIntf {
+	t := &taskImpl{
+		task: taskData,
+	}
+	t.ExtendBaseWidget(t)
+	t.initUI()
+	return t
+}
+
+func (t *taskImpl) initUI() {
+	synopsysText := ""
+	descriptionText := ""
+	if t.task != nil {
+		synopsysText = t.task.Data().Synopsis
+		descriptionText = t.task.Data().Description
+	}
+	t.synopsis = widget.NewLabelWithStyle(synopsysText, fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
+	t.description = widget.NewLabel(descriptionText)
+	t.container = container.NewVBox(t.synopsis, t.description)
+}
+
+func (t *taskImpl) Task() models.Task {
+	return t.task
+}
+
+func (t *taskImpl) SetTask(taskData models.Task) {
+	didChange := false
+	t.task = taskData
+	if taskData == nil {
+		return
+	}
+	if t.synopsis != nil {
+		t.synopsis.SetText(taskData.Data().Synopsis)
+		didChange = true
+	}
+	if t.description != nil {
+		t.description.SetText(taskData.Data().Description)
+		didChange = true
+	}
+	if didChange {
+		t.Refresh()
+	}
+}
+
+func (t *taskImpl) CreateRenderer() fyne.WidgetRenderer {
+	return widget.NewSimpleRenderer(t.container)
+}
