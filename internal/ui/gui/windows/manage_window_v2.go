207b9fb06d0ecb6bc590f655dea0e6c73a085546
alan lew <alan@ethereal.cc>
2022-12-09 18:36:57 -0500

    Add more to manage window v2

--- internal/ui/gui/windows/manage_window_v2.go
+++ internal/ui/gui/windows/manage_window_v2.go
@@ -2,7 +2,10 @@ package windows
 
 import (
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 	"github.com/neflyte/timetracker/internal/logger"
+	"github.com/neflyte/timetracker/internal/ui/gui/widgets"
 	"github.com/rs/zerolog"
 )
 
@@ -13,12 +16,18 @@ type manageWindowV2 interface {
 	Close()
 }
 
+var _ fyne.Window = (*manageWindowV2Impl)(nil)
+
 type manageWindowV2Impl struct {
 	fyne.Window
-	log        zerolog.Logger
-	app        *fyne.App
-	container  *fyne.Container
-	buttonHBox *fyne.Container
+	log          zerolog.Logger
+	app          *fyne.App
+	container    *fyne.Container
+	buttonHBox   *fyne.Container
+	createButton *widget.Button
+	editButton   *widget.Button
+	deleteButton *widget.Button
+	taskSelector *widgets.TaskSelector
 }
 
 func newManageWindowV2(app fyne.App) manageWindowV2 {
@@ -35,5 +44,36 @@ func newManageWindowV2(app fyne.App) manageWindowV2 {
 }
 
 func (m *manageWindowV2Impl) Init() error {
+	m.createButton = widget.NewButton("New", func() {})
+	m.editButton = widget.NewButton("Edit", func() {})
+	m.deleteButton = widget.NewButton("Delete", func() {})
+	m.buttonHBox = container.NewHBox(m.createButton, widget.NewSeparator(), m.editButton, m.deleteButton)
+	m.taskSelector = widgets.NewTaskSelector()
+	m.taskSelector.Observable().ForEach(
+		func(item interface{}) {
+
+		},
+		func(err error) {
+
+		},
+		func() {
+
+		},
+	)
+	m.container = container.NewVBox(m.buttonHBox, m.taskSelector)
+	m.Window.SetCloseIntercept(m.Hide)
+	m.Window.SetContent(m.container)
 	return nil
+}
+
+func (m *manageWindowV2Impl) Hide() {
+	m.Window.Hide()
+}
+
+func (m *manageWindowV2Impl) Close() {
+	m.Window.Close()
+}
+
+func (m *manageWindowV2Impl) Show() {
+	m.Window.Show()
 }
