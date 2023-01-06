b38fec3bf38a44118517450832df50f7c39f5035
Alan Lew <alan@ethereal.cc>
2023-01-06 11:08:54 -0500

    Update script to generate macOS ICNS files; Move generated icons into assets/icons; Add macOS ICNS files

--- scripts/generate_icns.sh
+++ scripts/generate_icns.sh
@@ -44,6 +44,16 @@ if [[ -z "$2" ]]; then
     exit 1
 fi
 OUTPUT_PATH="$2"
+# look for inkscape
+INKSCAPE_CMD="inkscape"
+if ! hash "$INKSCAPE_CMD" &>/dev/null; then
+    if [[ -d "/Applications/Inkscape.app/Contents/MacOS" ]]; then
+        INKSCAPE_CMD="/Applications/Inkscape.app/Contents/MacOS/inkscape"
+    else
+        printf "*  error: cannot find inkscape; inkscape is required for image conversion"
+        exit 3
+    fi
+fi
 # create output path
 if ! mkdir -p "$OUTPUT_PATH"; then
     printf "*  error: cannot create output directory '%s'.\n" "$OUTPUT_PATH"
@@ -58,10 +68,14 @@ fi
 # loop through dimensions to create images
 for DIM_INDEX in "${!DIMENSIONS[@]}"; do
     DIMENSION=${DIMENSIONS[$DIM_INDEX]}
-    ICON_FILENAME="${BASE_NAME}_${DIMENSION_LABEL[$DIM_INDEX]}.png"
+    ICON_FILENAME="icon_${DIMENSION_LABEL[$DIM_INDEX]}.png"
     ICON_PATH="$ICONSET_PATH/$ICON_FILENAME"
     printf "creating %s\n" "$ICON_FILENAME"
-    svg2png -w "$DIMENSION" -h "$DIMENSION" "$INPUT_FILE" "$ICON_PATH" || true
+    $INKSCAPE_CMD -w "$DIMENSION" -h "$DIMENSION" "$INPUT_FILE" -o "$ICON_PATH" || true
 done
+# create ICNS file
+ICNS_PATH="$OUTPUT_PATH/$BASE_NAME.icns"
+printf "creating %s\n" "$BASE_NAME.icns"
+iconutil -c icns -o "$ICNS_PATH" "$ICONSET_PATH"
 # all done.
 printf "done.\n"
