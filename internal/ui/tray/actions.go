7cbbc8a60f2c66b58846c0ba391dc2c3261129ab
alan lew <alan@ethereal.cc>
2023-03-25 14:15:16 -0400

    Attempt to close timing hole w.r.t. systray submenu items not displaying at all

--- internal/ui/tray/actions.go
+++ internal/ui/tray/actions.go
@@ -9,28 +9,38 @@ import (
 )
 
 var (
-	// updateTSMutex     = sync.Mutex{}
 	actionLoopRunning = false
 )
 
 // actionLoop is a goroutine that periodically updates the running timesheet object in the appstate map
-func actionLoop(quitChannel chan bool) {
+func actionLoop(quitChannel chan bool, startChannel chan bool) {
 	log := logger.GetFuncLogger(trayLogger, "actionLoop")
 	if actionLoopRunning {
-		log.Warn().Msg("action loop is already running")
+		log.Warn().
+			Msg("action loop is already running")
 		return
 	}
-	log.Trace().Msg("starting actionLoop")
+	log.Trace().
+		Msg("actionLoop started")
 	actionLoopRunning = true
 	defer func() {
 		actionLoopRunning = false
+		log.Trace().
+			Msg("actionLoop stopped")
 	}()
+	log.Debug().
+		Msg("waiting for start channel")
+	<-startChannel
+	log.Debug().
+		Msg("received from start channel; starting loop")
 	for {
 		updateRunningTimesheet()
-		log.Trace().Msgf("delaying %d seconds until next action loop", constants.ActionLoopDelaySeconds)
+		log.Trace().
+			Msgf("delaying %d seconds until next action loop", constants.ActionLoopDelaySeconds)
 		select {
 		case <-quitChannel:
-			log.Trace().Msg("quit channel fired; exiting function")
+			log.Trace().
+				Msg("quit channel fired; exiting function")
 			return
 		case <-time.After(constants.ActionLoopDelaySeconds * time.Second):
 			break
@@ -41,39 +51,26 @@ func actionLoop(quitChannel chan bool) {
 // updateRunningTimesheet gets the latest running timesheet object and sets the appropriate status
 func updateRunningTimesheet() {
 	log := logger.GetFuncLogger(trayLogger, "updateRunningTimesheet")
-	// log.Trace().Msg("acquiring lock")
-	// updateTSMutex.Lock()
-	// log.Trace().Msg("lock acquired successfully")
-	// defer func() {
-	// 	log.Trace().Msg("releasing lock")
-	// 	updateTSMutex.Unlock()
-	// 	log.Trace().Msg("lock released successfully")
-	// }()
 	timesheets, err := models.NewTimesheet().SearchOpen()
 	if err != nil {
-		// appstate.SetRunningTimesheet(nil) // Reset running timesheet
 		runningTimesheet = nil // Reset running timesheet
-		log.Err(err).Msg("error getting running timesheet")
-		// appstate.SetLastError(err)
+		log.Err(err).
+			Msg("error getting running timesheet")
 		lastError = err
-		// appstate.SetLastState(constants.TimesheetStatusError)
 		lastState = constants.TimesheetStatusError
 	} else {
-		// appstate.SetLastError(nil)
 		lastError = nil
 		if len(timesheets) == 0 {
 			// No running task
-			log.Trace().Msg("there are no running tasks")
-			// appstate.SetRunningTimesheet(nil) // Reset running timesheet
+			log.Trace().
+				Msg("there are no running tasks")
 			runningTimesheet = nil // Reset running timesheet
-			// appstate.SetLastState(constants.TimesheetStatusIdle)
 			lastState = constants.TimesheetStatusIdle
 		} else {
 			// Running task...
-			log.Trace().Msgf("there are %d running tasks", len(timesheets))
-			// appstate.SetRunningTimesheet(&timesheets[0])
+			log.Trace().
+				Msgf("there are %d running tasks", len(timesheets))
 			runningTimesheet = &timesheets[0]
-			// appstate.SetLastState(constants.TimesheetStatusRunning)
 			lastState = constants.TimesheetStatusRunning
 		}
 	}
