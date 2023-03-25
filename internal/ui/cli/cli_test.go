391169ecad01b79bcd2323cc83fc4fc79e02dcfa
alan lew <alan@ethereal.cc>
2023-03-25 11:31:31 -0400

    Add unit tests for CLI timesheet functions

---
+++ internal/ui/cli/cli_test.go
@@ -0,0 +1,47 @@
+package cli
+
+import (
+	"os"
+	"testing"
+
+	"github.com/neflyte/timetracker/internal/models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+const (
+	TestDSN = "file:test.db?cache=shared&mode=memory"
+)
+
+func TestMain(m *testing.M) {
+	os.Exit(m.Run())
+}
+
+func MustOpenTestDB(t *testing.T) *gorm.DB {
+	db, err := gorm.Open(sqlite.Open(TestDSN), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Warn),
+	})
+	if err != nil {
+		t.Fatalf("error opening test db: %s", err)
+	}
+	err = db.AutoMigrate(new(models.TaskData), new(models.TimesheetData))
+	if err != nil {
+		t.Fatalf("error automigrating test db schema: %s", err)
+	}
+	return db
+}
+
+func CloseTestDB(t *testing.T, db *gorm.DB) {
+	if db != nil {
+		sqldb, err := db.DB()
+		if err != nil {
+			t.Logf("error getting sql.DB handle: %s\n", err)
+		} else {
+			err = sqldb.Close()
+			if err != nil {
+				t.Logf("error closing DB handle: %s\n", err)
+			}
+		}
+	}
+}
