adde912f018957755999163d4d8893b8511a8dfa
alan lew <alan@ethereal.cc>
2023-08-20 11:13:15 -0400

    Redesign compact UI to be primarily vertical instead of horizontal

--- internal/ui/gui/widgets/compact_ui.go
+++ internal/ui/gui/widgets/compact_ui.go
@@ -8,39 +8,69 @@ import (
 	"fyne.io/fyne/v2/widget"
 	"github.com/neflyte/timetracker/internal/logger"
 	"github.com/neflyte/timetracker/internal/models"
+	"github.com/reactivex/rxgo/v2"
 	"github.com/rs/zerolog"
 )
+
+const (
+	compactUICommandChanSize         = 2
+	compactUILastStartedTasksDefault = 10
+)
+
+// CompactUIStartTaskEvent represents an event which starts a task
+type CompactUIStartTaskEvent struct {
+	TaskSynopsis string
+	TaskIndex    int
+}
+
+// CompactUIManageEvent represents an event which opens the Manage window
+type CompactUIManageEvent struct{}
+
+// CompactUIReportEvent represents an event which opens the Report window
+type CompactUIReportEvent struct{}
+
+// CompactUIQuitEvent represents an event which exits the application
+type CompactUIQuitEvent struct{}
+
+// CompactUIStopTaskEvent represents an event which starts a task
+type CompactUIStopTaskEvent struct{}
+
+// CompactUISelectTaskEvent represents an event which opens the task selector
+type CompactUISelectTaskEvent struct{}
 
 var _ fyne.Widget = (*CompactUI)(nil)
 
 // CompactUI is a compact user interface for the main Timetracker window
 type CompactUI struct {
-	taskNameBinding    binding.String
-	elapsedTimeBinding binding.String
-	container          *fyne.Container
-	taskSelect         *widget.Select
-	startStopButton    *widget.Button
-	taskNameLabel      *widget.Label
-	elapsedTimeLabel   *widget.Label
-	ellipsisButton     *widget.Button
-	ellipsisMenu       *fyne.Menu
-	ellipsisPopup      *widget.PopUpMenu
-	selectedTask       models.Task
-	log                zerolog.Logger
-	taskList           []string
+	taskNameBinding         binding.String
+	elapsedTimeBinding      binding.String
+	container               *fyne.Container
+	taskSelect              *widget.Select
+	taskListBinding         binding.StringList
+	taskListBindingListener binding.DataListener
+	startStopButton         *widget.Button
+	taskNameLabel           *widget.Label
+	elapsedTimeLabel        *widget.Label
+	selectedTask            models.Task
+	commandChan             chan rxgo.Item
+	log                     zerolog.Logger
+	taskList                []string
 	widget.BaseWidget
 }
 
 // NewCompactUI creates a new instance of the compact user interface
 func NewCompactUI() *CompactUI {
 	compactui := &CompactUI{
-		log:                logger.GetStructLogger("CompactUi"),
+		log:                logger.GetStructLogger("CompactUI"),
 		taskList:           make([]string, 0),
+		taskListBinding:    binding.NewStringList(),
 		taskNameBinding:    binding.NewString(),
 		elapsedTimeBinding: binding.NewString(),
+		commandChan:        make(chan rxgo.Item, compactUICommandChanSize),
 	}
 	compactui.ExtendBaseWidget(compactui)
 	compactui.initUI()
+	compactui.initBindings()
 	return compactui
 }
 
@@ -50,39 +80,59 @@ func (c *CompactUI) initUI() {
 	c.startStopButton = widget.NewButtonWithIcon("", theme.MediaPlayIcon(), c.startStopButtonWasTapped)
 	c.taskNameLabel = widget.NewLabelWithData(c.taskNameBinding)
 	c.elapsedTimeLabel = widget.NewLabelWithData(c.elapsedTimeBinding)
-	c.ellipsisButton = widget.NewButtonWithIcon("", theme.MoreHorizontalIcon(), c.ellipsisButtonWasTapped)
-	c.container = container.NewHBox(
+	c.container = container.NewVBox(
 		c.taskSelect,
-		c.startStopButton,
-		c.taskNameLabel,
-		c.elapsedTimeLabel,
-		c.ellipsisButton,
+		container.NewHBox(
+			c.startStopButton,
+			c.taskNameLabel,
+			c.elapsedTimeLabel,
+		),
+		container.NewHBox(
+			widget.NewButtonWithIcon("Manage", theme.SettingsIcon(), c.manageButtonWasTapped),       // i18n
+			widget.NewButtonWithIcon("Report", theme.DocumentCreateIcon(), c.reportButtonWasTapped), // i18n
+			widget.NewButtonWithIcon("Quit", theme.LogoutIcon(), c.quitButtonWasTapped),             // i18n
+		),
 	)
-	manageItem := fyne.NewMenuItem("Manage", c.manageMenuItemWasTapped)
-	manageItem.Icon = theme.SettingsIcon()
-	reportItem := fyne.NewMenuItem("Reports", c.reportMenuItemWasTapped)
-	reportItem.Icon = theme.DocumentCreateIcon()
-	quitItem := fyne.NewMenuItem("Quit", c.quitMenuItemWasTapped)
-	quitItem.Icon = theme.LogoutIcon()
-	c.ellipsisMenu = fyne.NewMenu("", manageItem, reportItem, quitItem)
-	c.ellipsisPopup = widget.NewPopUpMenu(c.ellipsisMenu)
 }
 
-// RefreshTasks reloads the last n started tasks and updates the selection widget
-func (c *CompactUI) RefreshTasks() {
-	log := logger.GetFuncLogger(c.log, "RefreshTasks")
-	lastStartedTasks, err := models.NewTimesheet().LastStartedTasks(10)
+func (c *CompactUI) initBindings() {
+	c.taskListBindingListener = binding.NewDataListener(c.taskListWasUpdated)
+	c.taskListBinding.AddListener(c.taskListBindingListener)
+}
+
+// Observable returns an rxgo Observable for the widget's command channel
+func (c *CompactUI) Observable() rxgo.Observable {
+	return rxgo.FromEventSource(c.commandChan)
+}
+
+// refreshTasks reloads the last n started tasks and updates the selection widget
+func (c *CompactUI) refreshTasks() {
+	log := logger.GetFuncLogger(c.log, "refreshTasks")
+	lastStartedTasks, err := models.NewTimesheet().LastStartedTasks(compactUILastStartedTasksDefault)
 	if err != nil {
 		log.Err(err).
 			Msg("unable to get last started tasks")
 		return
 	}
-	lastStartedTaskNames := make([]string, 0)
+	lastStartedTaskNames := make([]string, len(lastStartedTasks)+1)
 	for idx := range lastStartedTasks {
-		lastStartedTaskNames = append(lastStartedTaskNames, lastStartedTasks[idx].Synopsis)
+		lastStartedTaskNames[idx] = lastStartedTasks[idx].Synopsis
 	}
-	lastStartedTaskNames = append(lastStartedTaskNames, "Other...") // i18n
+	lastStartedTaskNames[len(lastStartedTasks)] = "Other..." // i18n
 	c.taskList = lastStartedTaskNames
+	c.taskSelect.Options = c.taskList
+	c.taskSelect.Refresh()
+}
+
+func (c *CompactUI) taskListWasUpdated() {
+	log := logger.GetFuncLogger(c.log, "taskListWasUpdated")
+	taskList, err := c.taskListBinding.Get()
+	if err != nil {
+		log.Err(err).
+			Msg("unable to get task list from binding")
+		return
+	}
+	c.taskList = taskList
 	c.taskSelect.Options = c.taskList
 	c.taskSelect.Refresh()
 }
@@ -108,6 +158,7 @@ func (c *CompactUI) taskWasSelected(selection string) {
 
 func (c *CompactUI) startStopButtonWasTapped() {
 	log := logger.GetFuncLogger(c.log, "startStopButtonWasTapped")
+	// FIXME: send the appropriate event based on the state of the button
 	c.startStopButton.Disable()
 	defer c.startStopButton.Enable()
 	runningTimesheets, err := models.NewTimesheet().SearchOpen()
@@ -130,27 +181,25 @@ func (c *CompactUI) startStopButtonWasTapped() {
 		// error: need to select a task
 		return
 	}
-	// start task
-
+	// TODO: start task
 	// update button
 	c.startStopButton.SetIcon(theme.MediaStopIcon())
 }
 
-func (c *CompactUI) ellipsisButtonWasTapped() {
-	// log := logger.GetFuncLogger(c.log, "ellipsisButtonWasTapped")
-	c.ellipsisPopup.ShowAtPosition(c.ellipsisButton.Position())
+func (c *CompactUI) manageButtonWasTapped() {
+	c.commandChan <- rxgo.Of(CompactUIManageEvent{})
 }
 
-func (c *CompactUI) manageMenuItemWasTapped() {
-
+func (c *CompactUI) reportButtonWasTapped() {
+	c.commandChan <- rxgo.Of(CompactUIReportEvent{})
 }
 
-func (c *CompactUI) reportMenuItemWasTapped() {
-
+func (c *CompactUI) quitButtonWasTapped() {
+	c.commandChan <- rxgo.Of(CompactUIQuitEvent{})
 }
 
-func (c *CompactUI) quitMenuItemWasTapped() {
-
+func (c *CompactUI) otherTaskWasSelected() {
+	c.commandChan <- rxgo.Of(CompactUISelectTaskEvent{})
 }
 
 // CreateRenderer returns a new WidgetRenderer for this widget
